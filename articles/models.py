9308b440a2a277195df290eb3d0cbed9bec2e4d6
alimgee <mcgee.al@gmail.com>
2021-10-09 15:12:38 +0000

    Adding Slug to modale and urls

--- articles/models.py
+++ articles/models.py
@@ -4,10 +4,12 @@ from django.urls import reverse
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    slug = models.SlugField(null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('article_detail', args=[str(self.id)])
+       # return reverse('article_detail', args=[str(self.id)])
+       return reverse('article_detail', kwargs={'slug': self.slug}) # added to run slug
 
--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    #path('<int:pk>', ArticleDetailView.as_view(), name='article_detail'), # without slug
+    path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'), # using slug
     path('', ArticleListView.as_view(), name='article_list'),
 ]
