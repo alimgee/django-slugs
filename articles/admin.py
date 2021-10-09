312c981b9a3fb8044338c984a5fe62da886594f6
alimgee <mcgee.al@gmail.com>
2021-10-09 15:18:20 +0000

    Adjusting model and admin files to add prepopulated field to slug field

--- articles/admin.py
+++ articles/admin.py
@@ -4,5 +4,6 @@ from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'body',)
+    prepopulated_fields = {'slug': ('title',)} # allows the slug to be prepopulated with title field
 
 admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    slug = models.SlugField(null=True)
+    slug = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return self.title
